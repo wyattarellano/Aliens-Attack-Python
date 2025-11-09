b82306fa3e6bf399b2054784145cc5b5de56b2b3
wyattarellano <wyatt.m.arellano@gmail.com>
2025-11-08 21:01:46 -0500

    Day1

---
+++ main.py
@@ -0,0 +1,74 @@
+import pygame
+from typing import List
+pygame.init()
+
+#DATA DEFINITIONS
+'''
+A World is a (world Rocket Aliens Dir Shots Tics2shoot)
+A Rocket is a (rocket X)
+An Alien is a (alien Dir Health)
+A Shot is a (shot X Y)
+Dir is one of: "left" "right"
+Health is a natnum between 0 and 3
+Aliens(LOA) is a List of Alien
+Shots(LOS) is a List of Shot
+'''
+
+#Game Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+tick = pygame.time.Clock()
+tick_rate = 60  # frames per second
+
+#Color constants
+PINK = (255, 192, 203)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+
+#Rocket Constants
+ROCKET_Y = SCREEN_HEIGHT - 60
+
+#Shot Constants
+INIT_T2S = 0  # initial tics to shoot
+RESET_T2S = 60 #tics to wait between shots
+
+class world:
+    def __init__(self, rocket, aliens, dir, shots, tics2shoot):
+        self.rocket = rocket  # player rocket
+        self.aliens = aliens  # enemy aliens
+        self.dir = dir  # direction of aliens
+        self.shots = shots  # shots fired by player
+        self.tics2shoot = tics2shoot  # tics until player can shoot again
+
+
+class alien:
+    def __init__(self, x, y, health):
+        self.x = x  # x position of alien
+        self.y = y  # y position of alien
+        self.health = health  # health of alien - natnum between 0 and 3
+
+
+class rocket:
+    def __init__(self, x):
+        self.x = x  # x position of rocket since rocket does not move vertically
+
+    def __draw__(self, screen):
+        pygame.draw.rect(screen, PINK, (self.x, ROCKET_Y, 40, 20))
+
+
+class shot:
+    def __init__(self, x, y):
+        self.x = x  # x position of shot
+        self.y = y  # y position of shot
+
+#INITIAL VALUES
+INIT_LOA = [alien(50 + i * 30, 50, 3) for i in range(10)]
+INIT_ROCKET = rocket(SCREEN_WIDTH // 2)
+INIT_LOS = []
+INIT_WORLD = world(INIT_ROCKET, INIT_LOA, "right", INIT_LOS, INIT_T2S)
+
+
+
